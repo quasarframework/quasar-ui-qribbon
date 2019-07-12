97cee1a11aaef1d20248b3831109300581cb4247
webnoob <allangaunt@gmail.com>
2019-07-12 14:58:57 +0100

    fix (demo) Fix local package links for demo.

--- src/boot/qribbon.js
+++ src/boot/qribbon.js
@@ -1,4 +1,4 @@
-import QRibbon from 'quasar-app-extension-qribbon/src/component/QRibbon'
+import QRibbon from '@quasar/quasar-app-extension-qribbon/src/component/QRibbon'
 
 export default ({ Vue }) => {
   Vue.component('q-ribbon', QRibbon)
