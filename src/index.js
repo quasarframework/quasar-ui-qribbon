d01b6a4b7402ec6fbffb2f94205260a6ecd0b1d6
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-08-22 07:29:04 -0600

    fix: transpile dependencies on quasar-mixin-colorize

--- src/index.js
+++ src/index.js
@@ -13,17 +13,22 @@ const extendConf = function (conf) {
   // make sure boot & component files transpile for SSR
   conf.build.transpileDependencies.push(/quasar-app-extension-qribbon[\\/]src/)
 
-  // make sure qjumbotron css goes through webpack to avoid ssr issues
+  // qribbon is dependent on colorize mixin
+  conf.build.transpileDependencies.push(/quasar-mixin-colorize[\\/]src/)
+
+
+  // make sure qribbon css goes through webpack to avoid ssr issues
   conf.css.push('~@quasar/quasar-app-extension-qribbon/src/component/styl/common/glow.styl')
   conf.css.push('~@quasar/quasar-app-extension-qribbon/src/component/styl/horizontal/main.styl')
   conf.css.push('~@quasar/quasar-app-extension-qribbon/src/component/styl/vertical/main.styl')
   conf.css.push('~@quasar/quasar-app-extension-qribbon/src/component/styl/corner/main.styl')
+  conf.css.push('~quasar-mixin-colorize/src/qColors.styl')
   console.log(` App Extension (qribbon) Info: 'Adding ribbon-*.styl css reference to your quasar.conf.js'`)
 }
 
 module.exports = function (api) {
   // quasar compatibility check
-  api.compatibleWith('@quasar/app', '^1.0.0-beta.18')
+  api.compatibleWith('@quasar/app', '^1.0.0')
 
   // register JSON api
   // api.registerDescribeApi('QRibbon', './component/QRibbon.json')
