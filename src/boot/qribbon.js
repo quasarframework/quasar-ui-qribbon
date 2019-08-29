d4fdcc3a9c5b29cf4b50c8ecb3febad0e33ffda3
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-08-29 07:43:16 -0600

    fix: QRibbon needs Colorize AE (remove Colorize mixin)

--- src/boot/qribbon.js
+++ src/boot/qribbon.js
@@ -1,7 +1,5 @@
 import QRibbon from '@quasar/quasar-app-extension-qribbon/src/component/QRibbon'
-import { Colorize } from 'quasar-mixin-colorize'
 
 export default ({ Vue }) => {
   Vue.component('q-ribbon', QRibbon)
-  Vue.component(Colorize)
 }
